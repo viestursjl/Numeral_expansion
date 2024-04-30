0dd9d14b0fd711c2183df095ac6293da30d47a74
viestursjl <viesturs.j.lasmanis@gmail.com>
2024-04-30 13:29:48 +0300

    Pabeidzu statisko sintaktisko metodi.

--- syntax_test.py
+++ syntax_test.py
@@ -3,6 +3,7 @@
 import json
 import stanza
 from ast import literal_eval
+
 # import pynini
 # from pynini.lib.rewrite import top_rewrite
 #
@@ -39,7 +40,7 @@ def load_stats(num_type, head):
             if "Case=" in target_tok and "Gender=" in target_tok:
                 case = target_tok.split("Case=")[1][:3]
                 gender = target_tok.split("Gender=")[1][:3]
-                final = "+"+(case + gender).upper()
+                final = "+" + (case + gender).upper()
                 print(final)
                 return final
         line = f.readline()
@@ -51,7 +52,7 @@ def fixed_stat_method(token, sentence):
         tok = sentence.to_dict()[0][token]
         num_type = tok["feats"]
         head = "_"
-        head_id = int(tok["head"])-1
+        head_id = int(tok["head"]) - 1
         if head_id >= 0:
             head_tok = sentence.to_dict()[0][head_id]
             if "feats" in head_tok:
@@ -63,7 +64,7 @@ def fixed_stat_method(token, sentence):
 
 
 def stanza_decline(sentence, phase1):
-    nlp = stanza.Pipeline('lv', download_method=None)
+    nlp = stanza.Pipeline('lv')
     sentence = nlp(sentence)
 
     for x in range(len(phase1)):
@@ -73,42 +74,41 @@ def stanza_decline(sentence, phase1):
     return " ".join(phase1)
 
 
-def exp_tokens(part_exp):
-    sent = part_exp.split(" ")
+def exp_tokens(part_expanded):
+    sent = part_expanded.split(" ")
     merger = ""
 
-    owo = []
+    sent_tokens = []
     in_token = False
     for tok in sent:
         if tok.startswith("{{{"):
             in_token = True
         if not in_token:
-            owo.append(tok)
+            sent_tokens.append(tok)
         else:
             merger += tok + " "
         if tok.endswith("}}}"):
             merger = merger.strip()
-            owo.append(merger)
-            merger=""
+            sent_tokens.append(merger)
+            merger = ""
             in_token = False
-    return owo
+    return sent_tokens
 
 
 def syn_expand(sentence):
-    # sentence = sentence.replace("[", "\\[").replace("]", "\\]")
-    # phase1 = top_rewrite(sentence, fst_exp)
-    # phase1 = phase1.replace("\\[", "[").replace("\\]", "]")
-    #
-    # expandable_tokens = exp_tokens(phase1)
-    # print(expandable_tokens)
-    # exit(1)
-    #
-    # phase2 = stanza_decline(phase1)
-    # phase2 = phase2.replace("[", "\\[").replace("]", "\\]")
-    #
-    # phase3 = top_rewrite(phase2, fst_exp)
-    # phase3 = phase3.replace("\\[", "[").replace("\\]", "]")
+    sent = sentence.replace("[", "\\[").replace("]", "\\]")
+    phase1 = top_rewrite(sent, fst_exp)
+    phase1 = phase1.replace("\\[", "[").replace("\\]", "]")
+
+    expandable_tokens = exp_tokens(phase1)
+
+    phase2 = stanza_decline(sentence, expandable_tokens)
+    phase2 = phase2.replace("[", "\\[").replace("]", "\\]")
+
+    phase3 = top_rewrite(phase2, fst_exp)
+    phase3 = phase3.replace("\\[", "[").replace("\\]", "]")
     return
+
 
 # syn_expand(text)
 # load_stats("NumType=Card", "Case=Acc|Gender=Masc|Number=Plur")
